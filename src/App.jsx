cb12ca575b179784eb48f44c6962f63d73a2033d
Tuan Cao <caomanhtuanbsb@gmail.com>
2022-11-11 17:32:26 +0700

    Adjust card position layout form items

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ function App() {
   return (
     <>
       <div className="cards-wrapper">
+        <div className="card-back">
+          <img src={card_back_bg} alt="" className="card-back-bg" />
+          <span className="ccv"> 123</span>
+        </div>
         <div className="card-front">
           <img src={card_front_bg} alt="" className="card-front-bg" />
           <img src={card_logo} alt="" className="logo" />
@@ -20,11 +24,9 @@ function App() {
             <span className="card-number">0000</span>
           </div>
           <div className="name-row">
-            <span className="owner-name">Felicia Leire</span> <span className="expire-date">09/22</span>
+            <span className="owner-name">Felicia Leire</span>{" "}
+            <span className="expire-date">09/22</span>
           </div>
-        </div>
-        <div className="card-back">
-          <img src={card_back_bg} alt="" />
         </div>
       </div>
       <div className="container">
@@ -32,7 +34,40 @@ function App() {
           <img src={mobile_bg} className="mobile-image" />
           <img src={desktop_bg} className="desktop-image" />
         </div>
-        <div className="form-container"></div>
+        <div className="form-container">
+          <form action="/submit" method="post">
+            <div className="form-name-group">
+              <label htmlFor="card-hodler-name">Cardholder name</label>
+              <input
+                type="text"
+                name=""
+                id="owner-name"
+                placeholder="e.g Jane Appleseed"
+              />
+            </div>
+            <div className="form-card-number-group">
+              <label htmlFor="card-number">Card Number</label>
+              <input
+                type="text"
+                name=""
+                id="owner-name"
+                placeholder="e.g 1234 1231 1231 3123"
+              />
+            </div>
+            <div className="form-exp-date-group">
+              <label htmlFor="expire-date">EXP. Date (MM/YY)</label>
+              <div className="form-exp-date-input">
+                <input type="text" name="month" id="month" placeholder="MM" />
+                <input type="text" name="month" id="Year" placeholder="YY" />
+              </div>
+            </div>
+            <div className="form-cvc-group">
+              <label htmlFor="CVC">CCV</label>
+              <input type="text" name="CCV" id="ccv" />
+            </div>
+            <input type="button" value="Confirm" />
+          </form>
+        </div>
       </div>
     </>
   )
